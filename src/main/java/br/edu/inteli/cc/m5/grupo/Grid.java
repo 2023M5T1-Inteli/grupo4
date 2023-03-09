9306285a94d264e3c7581e1b0b7ed576f71dd8d9
SergioLucasINT <sergio.lucas@sou.inteli.edu.br>
2023-03-09 14:40:00 -0300

    Print de coordenadas

--- src/main/java/br/edu/inteli/cc/m5/grupo/Grid.java
+++ src/main/java/br/edu/inteli/cc/m5/grupo/Grid.java
@@ -25,32 +25,34 @@ public class Grid {
     private void postNodes(){
         List<LatLon> topLine = Intermediary_Points.calculateIntermediaryPoints(LatRegInit, LonRegInit, LatRegEnd, LonRegInit);
         List<LatLon> bottomLine = Intermediary_Points.calculateIntermediaryPoints(LatRegEnd, LonRegInit, LatRegEnd, LonRegEnd);
-
+    
         int gridLength = topLine.size();
         double[][] getLatLon = new double[gridLength][4];
         LatLon currentPoint;
-
+    
         for(int i = 0; i < gridLength; i++){
-
+    
             currentPoint = topLine.get(i);
             getLatLon[i][0] = currentPoint.latitude;
             getLatLon[i][1] = currentPoint.longitude;
-
+    
             currentPoint = bottomLine.get(i);
             getLatLon[i][2] = currentPoint.latitude;
             getLatLon[i][3] = currentPoint.longitude;
-
+    
         }
-
+    
         for(int j = 0; j < gridLength; j++){
-            Intermediary_Points.calculateIntermediaryPoints(getLatLon[j][0], getLatLon[j][1], getLatLon[j][2], getLatLon[j][3]);
-        }
-
-        private void postRelationship(){
-
+            List<LatLon> intermediaryPoints = Intermediary_Points.calculateIntermediaryPoints(getLatLon[j][0], getLatLon[j][1], getLatLon[j][2], getLatLon[j][3]);
+            for (LatLon point : intermediaryPoints) {
+                System.out.println("[" + point.latitude + "] [" + point.longitude + "]");
+            }
         }
     }
+    
     public static void main(String[] args) {
 
+        Grid malha = new Grid(-22.5889042043, -43.4855748, -22.359194448201, -42.5794347619519);
+        
     }
 }
